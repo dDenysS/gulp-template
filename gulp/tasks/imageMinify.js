f6f0a9bee59d6980fbf1c35b8ea1d1de26a445d5
sashadeeep <alexandervatamanyuk@gmail.com>
2020-11-15 11:43:35 +0200

    - fixed image-minify gulp-task for build without crashing - fixed styles gulp-task for correctly mapping of stylesheets

--- gulp/tasks/imageMinify.js
+++ gulp/tasks/imageMinify.js
@@ -3,20 +3,7 @@ const imagemin = require('gulp-imagemin')
 
 module.exports = function imageMinify() {
   return gulp.src('src/img/*.{gif,png,jpg,svg,webp}')
-    .pipe(imagemin([
-      imagemin.gifsicle({ interlaced: true }),
-      imagemin.mozjpeg({
-        quality: 75,
-        progressive: true
-      }),
-      imagemin.optipng({ optimizationLevel: 5 }),
-      imagemin.svgo({
-        plugins: [
-          { removeViewBox: true },
-          { cleanupIDs: false }
-        ]
-      })
-    ]))
+    .pipe(imagemin())
     .pipe(gulp.dest('build/img'))
 }
 
